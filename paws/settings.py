f80ea9c79d9920f5bb37bfb5d21200df217fafc0
Adrian Wang <adrianjwang@gmail.com>
2017-01-08 02:00:36 -0800

    modify proxy single from 5 to 50

--- paws/settings.py
+++ paws/settings.py
@@ -1,7 +1,7 @@
 USERAGENT_SERVICE_URL = 'http://localhost:5001/useragent/'
 
 #PROXY_SERVICE_URL = 'http://localhost:5002/proxy/'
-PROXY_SERVICE_URL = 'http://www.httpdaili.com/api.asp?ddbh=2804450698753473&old=1&noinfo=true&sl=5&nm=1'
+PROXY_SERVICE_URL = 'http://www.httpdaili.com/api.asp?ddbh=2804450698753473&old=1&noinfo=true&sl=50&nm=1'
 
 TASKS_TOTAL = 'scheduler:tasks:perday'
 
